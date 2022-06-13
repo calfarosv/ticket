d3a4e4abb783a2e122bf21628d81875607b4d986
calfarosv <calfaro@cel.gob.sv>
2022-06-13 15:57:53 -0600

    13.06.2022 15:57

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -49,4 +49,139 @@ export class TicketController {
 
     }
 
+    //-------------------------------------------------------------------------------------------------------------
+
+    @Get('/by_prioridad/:rti_prioridad')
+    @ApiOperation({ summary: 'Consulta de Tickets por prioridad' })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Consulta de Tickets por prioridad',
+        type: [Css_Rti_Entity],
+    })
+    async obtiene_ticket_por_prioridad(
+        @Param('rti_prioridad') v_rti_prioridad: string,
+    ) {
+        let v_rti_caso = '01';
+        let v_rti_codcia = '001';
+        let v_rti_codigo: number = null;
+        //let v_rti_prioridad = '';
+        let v_rti_coduniresp: number = null;
+        let v_rti_codemp = '';
+        let v_rti_codsis: number = null;
+        let v_rti_codmsi: number = null;
+        let v_rti_estado = '';
+        let v_rti_feccrea: Date;
+        let v_rti_fecsol: Date;
+        let v_rti_fecfin: Date;
+        let v_rti_anisol: number = null;
+        let v_rti_codsol: number = null;
+
+        const data: any[] = await this.ticketService.busca_ticket_dinamico(
+            v_rti_caso,
+            v_rti_codcia,
+            v_rti_codigo,
+            v_rti_prioridad,
+            v_rti_coduniresp,
+            v_rti_codemp,
+            v_rti_codsis,
+            v_rti_codmsi,
+            v_rti_estado,
+            v_rti_feccrea,
+            v_rti_fecsol,
+            v_rti_fecfin,
+            v_rti_anisol,
+            v_rti_codsol);
+        //console.log('data: ', data);
+        // VERIFICO SI LOS DATOS OBTENIDOS SON VARIOS REGISTROS, SOLO UNO O NINGUNO
+        // PRIMERO SE DEFINE SI LOS DATOS SON UN ARREGLO O NO
+        if (Array.isArray(data)) {
+            //console.log('Es un arreglo');
+            //SI ES UN ARREGLO, SE DEVUELVE CADA REGISTRO COMO JASON (LOS CORCHETES SON AUTOMATICOS POR SER ARREGLO)
+            return data
+        }
+        else {
+            //console.log('NO es un arreglo');
+            //SI NO ES UN ARREGLO, VERIFICO SI LOS DATOS VIENEN VACIOS O NO
+            if (!data) {
+                //SI LOS DATOS VIENEN VACIOS, SE DEVUELVEN CORCHETES
+                return []
+            }
+            else {
+                // SI LOS DATOS NO VIENEN VACIOS, ENTONCES ES SOLO 1 REGISTRO
+                // SE DEVUELVE ENTRE CORCHETES
+                return [data];
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------
+
+    @Get('/by_coduniresp/:rti_coduniresp')
+    @ApiOperation({ summary: 'Consulta de Tickets por Código de la Unidad Responsable' })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Consulta de Tickets por Código de la Unidad Responsable',
+        type: [Css_Rti_Entity],
+    })
+    async obtiene_ticket_por_coduniResp(
+        @Param('rti_coduniresp') v_rti_coduniresp: number,
+    ) {
+        //
+        let v_rti_caso = '02';
+        //
+        let v_rti_codcia = '001';
+        let v_rti_codigo: number = null;
+        let v_rti_prioridad = '';
+        //let v_rti_coduniresp: number = null;
+        let v_rti_codemp = '';
+        let v_rti_codsis: number = null;
+        let v_rti_codmsi: number = null;
+        let v_rti_estado = '';
+        let v_rti_feccrea: Date;
+        let v_rti_fecsol: Date;
+        let v_rti_fecfin: Date;
+        let v_rti_anisol: number = null;
+        let v_rti_codsol: number = null;
+
+        const data: any[] = await this.ticketService.busca_ticket_dinamico(
+            v_rti_caso,
+            v_rti_codcia,
+            v_rti_codigo,
+            v_rti_prioridad,
+            v_rti_coduniresp,
+            v_rti_codemp,
+            v_rti_codsis,
+            v_rti_codmsi,
+            v_rti_estado,
+            v_rti_feccrea,
+            v_rti_fecsol,
+            v_rti_fecfin,
+            v_rti_anisol,
+            v_rti_codsol);
+        //console.log('data: ', data);
+        // VERIFICO SI LOS DATOS OBTENIDOS SON VARIOS REGISTROS, SOLO UNO O NINGUNO
+        // PRIMERO SE DEFINE SI LOS DATOS SON UN ARREGLO O NO
+        if (Array.isArray(data)) {
+            //console.log('Es un arreglo');
+            //SI ES UN ARREGLO, SE DEVUELVE CADA REGISTRO COMO JASON (LOS CORCHETES SON AUTOMATICOS POR SER ARREGLO)
+            return data
+        }
+        else {
+            //console.log('NO es un arreglo');
+            //SI NO ES UN ARREGLO, VERIFICO SI LOS DATOS VIENEN VACIOS O NO
+            if (!data) {
+                //SI LOS DATOS VIENEN VACIOS, SE DEVUELVEN CORCHETES
+                return []
+            }
+            else {
+                // SI LOS DATOS NO VIENEN VACIOS, ENTONCES ES SOLO 1 REGISTRO
+                // SE DEVUELVE ENTRE CORCHETES
+                return [data];
+            }
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------------   
+
+
+
 } // export class
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -25,11 +25,12 @@ export class TicketService {
         return register;
     }
 
+
     @ApiHeader({
         name: 'Servicio: busca Ticket por la llave primaria',
         description: 'Maestro de Tickets',
     })
-    async obtiene_Tickets_byPk(v_codcia: string, v_codigo:number): Promise<Css_Rti_Entity> {
+    async obtiene_Tickets_byPk(v_codcia: string, v_codigo: number): Promise<Css_Rti_Entity> {
         const register = await this.ticketRepository.findOne(
             {
                 rtiCodcia: v_codcia,
@@ -39,4 +40,102 @@ export class TicketService {
         return register;
     }
 
-}
+
+    //-------------------------------------------------------------------------------------------------------------
+
+    @ApiHeader({
+        name: 'Servicio: Busqueda dinamica de Tickets',
+        description: 'Maestro de Tickets',
+    })
+    async busca_ticket_dinamico(
+        v_rti_caso: string,
+        v_rti_codcia: string,
+        v_rti_codigo: number,
+        v_rti_prioridad: string,
+        v_rti_coduniresp: number,
+        v_rti_codemp: string,
+        v_rti_codsis: number,
+        v_rti_codmsi: number,
+        v_rti_estado: string,
+        v_rti_feccrea: Date,
+        v_rti_fecsol: Date,
+        v_rti_fecfin: Date,
+        v_rti_anisol: number,
+        v_rti_codsol: number) {
+        //console.log('v_codunisol: ', v_codunisol);
+        //console.log('v_codunieje: ', v_codunieje);
+        //console.log('v_codcelres: ', v_codcelres);
+
+        let v_where = '';
+
+        if (v_rti_caso = '01') {
+            v_where = 'Css_Rti_Entity.rtiCodcia = :par_rti_codcia and Css_Rti_Entity.rtiPrioridad = :par_rti_prioridad';
+            //console.log('1', v_where);
+        }
+        if (v_rti_caso = '02') {
+            v_where = 'Css_Rti_Entity.rtiCodcia = :par_rti_codcia and Css_Rti_Entity.rtiCoduniResp = :par_rti_coduniresp';
+            //console.log('1', v_where);
+        }
+
+        const register = await this.ticketRepository.createQueryBuilder()
+            .select('Css_Rti_Entity.rtiCodcia', 'rtiCodcia')
+            .addSelect('Css_Rti_Entity.rtiCodigo', 'rtiCodigo')
+            .addSelect('Css_Rti_Entity.rtiDescripcion', 'rtiDescripcion')
+            .addSelect('Css_Rti_Entity.rtiResultado', 'rtiResultado')
+            .addSelect('Css_Rti_Entity.rtiPrioridad', 'rtiPrioridad')
+            .addSelect('Css_Rti_Entity.rtiCoduniResp', 'rtiCoduniResp')
+            .addSelect('Css_Rti_Entity.rtiCodemp', 'rtiCodemp')
+            .addSelect('Css_Rti_Entity.rtiUsrred', 'rtiUsrred')
+            .addSelect('Css_Rti_Entity.rtiCorreo', 'rtiCorreo')
+            .addSelect('Css_Rti_Entity.rtiNavega', 'rtiNavega')
+            .addSelect('Css_Rti_Entity.rtiSistema', 'rtiSistema')
+            .addSelect('Css_Rti_Entity.rtiCodsis', 'rtiCodsis')
+            .addSelect('Css_Rti_Entity.rtiCodmsi', 'rtiCodmsi')
+            .addSelect('Css_Rti_Entity.rtiEstado', 'rtiEstado')
+            .addSelect('Css_Rti_Entity.rtiFeccrea', 'rtiFeccrea')
+            .addSelect('Css_Rti_Entity.rtiFecanula', 'rtiFecanula')
+            .addSelect('Css_Rti_Entity.rtiFecsol', 'rtiFecsol')
+            .addSelect('Css_Rti_Entity.rtiFecfin', 'rtiFecfin')
+            .addSelect('Css_Rti_Entity.rtiAnisol', 'rtiAnisol')
+            .addSelect('Css_Rti_Entity.rtiCoduni', 'rtiCoduni')
+            .addSelect('Css_Rti_Entity.rtiCodsol', 'rtiCodsol')
+            //
+            //.addSelect('Pla_Uni_Unidad_Entity.uniNombre', 'uniNombre_sol')
+            //.addSelect('Pla_Uni_Unidad_Entity_b.uniNombre', 'uniNombre_eje')
+            //.addSelect('Pri_Emp_Empleado_V_Entity.empNombre', 'empNombre_res')
+            //
+            .where(v_where,
+                {
+                    par_rti_codcia: v_rti_codcia,
+                    par_rti_codigo: v_rti_codigo,
+                    par_rti_prioridad: v_rti_prioridad,
+                    par_rti_coduniresp: v_rti_coduniresp,
+                    par_rti_codemp: v_rti_codemp,
+                    par_rti_codsis: v_rti_codsis,
+                    par_rti_codmsi: v_rti_codmsi,
+                    par_rti_estado: v_rti_estado,
+                    par_rti_feccrea: v_rti_feccrea,
+                    par_rti_fecsol: v_rti_fecsol,
+                    par_rti_fecfin: v_rti_fecfin,
+                    par_rti_anisol: v_rti_anisol,
+                    par_rti_codsol: v_rti_codsol
+                })
+            //.leftJoin(Pri_Emp_Empleado_V_Entity, 'Pri_Emp_Empleado_V_Entity', 'Pri_Fic_Ficha_Entity.ficCodcelRes = Pri_Emp_Empleado_V_Entity.empCodcel')
+            //.leftJoin(Pla_Uni_Unidad_Entity, 'Pla_Uni_Unidad_Entity', 'Pri_Fic_Ficha_Entity.ficCoduniSol = Pla_Uni_Unidad_Entity.uniCodigo')
+            //.leftJoin(Pla_Uni_Unidad_Entity, 'Pla_Uni_Unidad_Entity_b', 'Pri_Fic_Ficha_Entity.ficCoduniEje = Pla_Uni_Unidad_Entity_b.uniCodigo')
+            .getRawMany();
+        //console.log('register: ', register);            
+        /*
+        if (!register || register.length === 0) {
+            throw new HttpException('No se encontraron datos - (busca_fichas_dinamica)', HttpStatus.FORBIDDEN);
+        }
+        else
+            return register;
+        */
+        return register;
+    }
+
+    //-------------------------------------------------------------------------------------------------------------
+
+
+} // export class
